1cd0e216706d9d1c9537c3b655df01091e72634c
Prince-Sondagar <sondagarprince1@gmail.com>
2024-11-13 22:22:28 +0530

    created the appointmwent related page

---
+++ app/(dashboard)/appointment/create/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Metadata from "@/app/components/metadata";
+
+const AppointmentCreate = () => {
+
+    return (
+        <>
+            <Metadata title="Appointment Create" />
+
+            <h1>Appointment Create</h1>
+        </>
+
+    )
+};
+
+export default AppointmentCreate;
---
+++ app/(dashboard)/appointment/lists/page.tsx
@@ -0,0 +1,17 @@
+
+"use client";
+
+import Metadata from "@/app/components/metadata";
+
+const AppointmentLists = () => {
+
+    return (
+        <>
+            <Metadata title="Appointment Lists" />
+            <h1>Appointment Lists</h1>
+
+        </>
+    )
+};
+
+export default AppointmentLists;
